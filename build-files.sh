e875b7e701abb3e45228b19a568f1ab66201b3c5
FabianCv <fcampoverdev@unprg.edu.pe>
2024-03-28 11:23:59 -0500

    Vercel App Configuration5

--- build-files.sh
+++ build-files.sh
@@ -1,8 +1,1 @@
-# Crear un entorno virtual (si no está creado ya)
-python3 -m venv venv
-
-# Activar el entorno virtual
-source venv/bin/activate
-
-# Instalar las dependencias del proyecto
 pip install -r requirements.txt
